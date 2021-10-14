d543d1a1597d95a4e02be8babeb5de98ff54ad39
Carlos <carlostena@centroafuera.onmicrosoft.com>
2021-10-14 14:21:41 +0200

    Initial commit

---
+++ src/main/java/Hilo.java
@@ -0,0 +1,20 @@
+public class Hilo extends Thread {
+
+    public String nombre;
+
+    public Hilo(String nombre){
+        this.nombre = nombre;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Soy el hilo " + nombre + " y me voy a dormir.");
+        try {
+            Thread.sleep(Main.getRandomTimeInMills());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Soy el hilo " + nombre + "y me he despertado.");
+
+    }
+}
